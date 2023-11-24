8dcf512946255036adf7142a2723b3ea4512923c
Vinicius Batista Alves Salgado <vinicius.salgado@external.stellantis.com>
2023-11-24 00:51:34 -0300

    FEAT: Page created

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; // Importing from 'react-dom/client'
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
 root.render(
   <React.StrictMode>
     <App />
